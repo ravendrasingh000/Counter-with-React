570953f5c4ffaea151698df7f7ac5707c2b8f359
Ravendra Singh <ravendra24@navgurukul.org>
2025-07-31 23:57:51 +0530

    color change if count 0 or less then 0

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 const App = () => {
   const [count, setCount] = useState(0);
 
+   const headingStyle = {
+    color: count <= 0 ? "red" : "black"
+  };
+
   return (
     <div id="container">
-      <h1>{count}</h1>
+      <h1 style={headingStyle}>{count}</h1>
       <div id="btn">
         <button onClick={() => setCount(count - 1)}>Decrement</button>
         <button onClick={() => setCount(count + 1)}>Increment</button>
