23196f68b607c1e3ee566c3ae09e432da190fbe6
Ravendra Singh <ravendra24@navgurukul.org>
2025-08-01 02:06:25 +0000

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Form from './Form'
 import Counter from './assets/Components/Counter'
 
 const App = () => {
   return (
     <>
-    {/* <Counter/> */}
-    <Form/>
+    <Counter/>
     </>
   )
 }
